dbc071a651aee5003b2399c2f0f6460ceb0d08f8
benisamuel <benisamuel566@gmail.com>
2025-05-06 20:40:53 +0200

    Initial Commit

---
+++ src/main/java/com/b4music/b4core/B4coreApplication.java
@@ -0,0 +1,13 @@
+package com.b4music.b4core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B4coreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B4coreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/b4music/b4core/B4coreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b4music.b4core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B4coreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
