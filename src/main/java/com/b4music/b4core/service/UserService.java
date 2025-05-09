b1356e6b23796dacc415b5e2fe15c144c18e0468
benisamuel <benisamuel566@gmail.com>
2025-05-09 20:22:18 +0200

    Added userService

--- src/main/java/com/b4music/b4core/service/UserService.java
+++ src/main/java/com/b4music/b4core/service/UserService.java
@@ -1,4 +1,73 @@
 package com.b4music.b4core.service;
 
+import com.b4music.b4core.dto.UserRegisterDto;
+import com.b4music.b4core.model.User;
+import com.b4music.b4core.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService (UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public List<User> getAllUsers () {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getUsersByEmail (String email) {
+        return this.userRepository.getUsersByEmail(email);
+    }
+
+    public Page<User> getAllUsersPaginated (int page, int size, String sortBy) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return this.userRepository.findAll(pageable);
+    }
+
+    public User getUserById (Long id) {
+        return this.userRepository.getUserById(id).orElse(null);
+    }
+
+    public User getUserByEmail (String email) {
+        return this.userRepository.getUserByEmail(email).orElse(null);
+    }
+
+    public User createUser (UserRegisterDto userRegisterDto) {
+        User newUser = new User(userRegisterDto.getName(), userRegisterDto.getEmail(), passwordEncoder.encode(userRegisterDto.getPassword()), userRegisterDto.getRole());
+        return this.userRepository.save(newUser);
+    }
+
+    public User updateUserById (Long id, UserRegisterDto userRegisterDto) {
+        User user = this.getUserById(id);
+        if (user != null) {
+            user.setName(userRegisterDto.getName());
+            user.setEmail(userRegisterDto.getEmail());
+            user.setPassword(userRegisterDto.getPassword());
+            user.setPhone(user.getPhone());
+            user.setRole(userRegisterDto.getRole());
+
+            return this.userRepository.save(user);
+        }
+        return null;
+    }
+
+    public boolean deleteUserById (Long id) {
+        User user = this.getUserById(id);
+        if (user != null) {
+            this.userRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
