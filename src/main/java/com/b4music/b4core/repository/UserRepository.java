7705d220060a73eefe7ede7cbdb69019903a603f
benisamuel <benisamuel566@gmail.com>
2025-05-09 20:22:45 +0200

    Updated userRepository

--- src/main/java/com/b4music/b4core/repository/UserRepository.java
+++ src/main/java/com/b4music/b4core/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.b4music.b4core.repository;
 import com.b4music.b4core.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
    Optional<User> getUserByEmail (String email);
-   Optional<User> getUserBYId (Long id);
+   Optional<User> getUserById (Long id);
+   List<User> getUsersByEmail (String email);
 }
