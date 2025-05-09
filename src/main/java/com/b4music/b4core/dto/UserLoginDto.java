a5dc8689ecf623b5dcfc40c2bee1fa9c77461c81
benisamuel <benisamuel566@gmail.com>
2025-05-09 20:24:12 +0200

    Added UserDto for login and register

--- src/main/java/com/b4music/b4core/dto/UserLoginDto.java
+++ src/main/java/com/b4music/b4core/dto/UserLoginDto.java
@@ -1,4 +1,14 @@
 package com.b4music.b4core.dto;
 
 public class UserLoginDto {
+    private String email;
+    private String password;
+
+    public String getEmail () {
+        return this.email;
+    }
+
+    public String getPassword () {
+        return this.password;
+    }
 }
--- src/main/java/com/b4music/b4core/dto/UserRegisterDto.java
+++ src/main/java/com/b4music/b4core/dto/UserRegisterDto.java
@@ -1,4 +1,26 @@
 package com.b4music.b4core.dto;
 
+import com.b4music.b4core.enums.Role;
+
 public class UserRegisterDto {
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
+
+    public String getName () {
+        return this.name;
+    }
+
+    public String getEmail () {
+        return this.email;
+    }
+
+    public String getPassword () {
+        return this.password;
+    }
+
+    public Role getRole () {
+        return this.role;
+    }
 }
