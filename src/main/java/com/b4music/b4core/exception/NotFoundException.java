0011a250dfe3952754cd312014e1474341e23b13
benisamuel <benisamuel566@gmail.com>
2025-05-09 20:23:34 +0200

    Added Exception

--- src/main/java/com/b4music/b4core/exception/NotFoundException.java
+++ src/main/java/com/b4music/b4core/exception/NotFoundException.java
@@ -1,4 +1,11 @@
 package com.b4music.b4core.exception;
 
-public class NotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+    public NotFoundException (String msg) {
+        super(msg);
+    }
 }
