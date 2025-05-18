a34ef45a995d7ef4f185dc6806c200dbd78fb852
benisamuel <benisamuel566@gmail.com>
2025-05-18 21:50:31 +0200

    Added reelService

--- src/main/java/com/b4music/b4core/service/ReelService.java
+++ src/main/java/com/b4music/b4core/service/ReelService.java
@@ -1,5 +1,6 @@
 package com.b4music.b4core.service;
 
+import com.b4music.b4core.dto.ReelDto;
 import com.b4music.b4core.model.Reels;
 import com.b4music.b4core.model.User;
 import com.b4music.b4core.repository.ReelsRepository;
@@ -28,4 +29,44 @@ public class ReelService {
         }
         return null;
     }
+
+    public Reels getReelsById (Long id) {
+        return this.reelsRepository.getReelsById(id);
+    }
+
+
+    public Reels createReel (ReelDto reelDto) {
+        User user = this.userService.getUserById(reelDto.getUser_id());
+        if (user != null) {
+            Reels newReels = new Reels(reelDto.getName(), reelDto.getDescription(), reelDto.getLikes(), reelDto.getDislikes(), user);
+            return this.reelsRepository.save(newReels);
+        }
+        return null;
+    }
+
+    public Reels updateReel (Long id, ReelDto reelDto) {
+        User user = this.userService.getUserById(reelDto.getUser_id());
+        Reels reel = this.getReelsById(id);
+
+        if (user != null && reel != null) {
+            reel.setName(reelDto.getName());
+            reel.setDescription(reelDto.getDescription());
+            reel.setLikes(reelDto.getLikes());
+            reel.setDislikes(reel.getDislikes());
+            reel.setUser(user);
+
+            return this.reelsRepository.save(reel);
+        }
+        return null;
+    }
+
+    public Boolean deleteReel (Long id) {
+        Reels reels = this.getReelsById(id);
+        if (reels != null) {
+            this.reelsRepository.delete(reels);
+            return true;
+        }
+        return false;
+    }
+
 }
