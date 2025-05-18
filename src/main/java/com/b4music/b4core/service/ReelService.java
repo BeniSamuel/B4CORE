892c9a842d8d2fe562a1d218b597c683bf2fed77
benisamuel <benisamuel566@gmail.com>
2025-05-18 20:22:32 +0200

    Added reelService

--- src/main/java/com/b4music/b4core/service/ReelService.java
+++ src/main/java/com/b4music/b4core/service/ReelService.java
@@ -1,4 +1,31 @@
 package com.b4music.b4core.service;
 
+import com.b4music.b4core.model.Reels;
+import com.b4music.b4core.model.User;
+import com.b4music.b4core.repository.ReelsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReelService {
+    private final ReelsRepository reelsRepository;
+    private final UserService userService;
+
+    public ReelService (ReelsRepository reelsRepository, UserService userService) {
+        this.reelsRepository = reelsRepository;
+        this.userService = userService;
+    }
+
+    public List<Reels> getAllReels () {
+        return this.reelsRepository.findAll();
+    }
+
+    public List<Reels> getAllReelsByUser (Long userId) {
+        User user = this.userService.getUserById(userId);
+        if (user != null) {
+            return this.reelsRepository.getReelsByUser(user);
+        }
+        return null;
+    }
 }
