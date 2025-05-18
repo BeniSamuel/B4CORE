9a3fd11836ed7993f8501a62666ab94c85af71ae
benisamuel <benisamuel566@gmail.com>
2025-05-18 22:09:15 +0200

    added music repository

--- src/main/java/com/b4music/b4core/repository/MusicRepository.java
+++ src/main/java/com/b4music/b4core/repository/MusicRepository.java
@@ -1,7 +1,11 @@
 package com.b4music.b4core.repository;
 
 import com.b4music.b4core.model.Music;
+import com.b4music.b4core.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MusicRepository extends JpaRepository<Music, Long> {
+    List<Music> getMusicByUser(User user);
 }
