70bd5165bd17f5edaa0cf6a112419b0da306bd70
benisamuel <benisamuel566@gmail.com>
2025-05-18 21:35:31 +0200

    Added constructor in reel model

--- src/main/java/com/b4music/b4core/model/Reels.java
+++ src/main/java/com/b4music/b4core/model/Reels.java
@@ -2,19 +2,15 @@ package com.b4music.b4core.model;
 
 import jakarta.persistence.*;
 
-import java.util.List;
-
 @Entity
 public class Reels {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String name;
     private String description;
 
-    @OneToMany
-    private List<Comment> comments;
     private Long likes;
     private Long dislikes;
 
@@ -22,12 +18,15 @@ public class Reels {
     @JoinColumn(name = "user_id")
     private User user;
 
-    public void setUser(User user) {
+    public Reels () {}
+    public Reels (String name, String description, Long likes, Long dislikes, User user) {
+        this.name = name;
+        this.description = description;
+        this.likes = likes;
+        this.dislikes = dislikes;
         this.user = user;
     }
-    public List<Comment> getComments() {
-        return comments;
-    }
+
     public String getName() {
         return name;
     }
@@ -44,9 +43,8 @@ public class Reels {
         return user;
     }
 
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
+    public void setUser(User user) {
+        this.user = user;
     }
     public void setDescription(String description) {
         this.description = description;
@@ -54,5 +52,10 @@ public class Reels {
     public void setName(String name) {
         this.name = name;
     }
-
+    public void setLikes(Long likes) {
+        this.likes = likes;
+    }
+    public void setDislikes (Long dislikes) {
+        this.dislikes = dislikes;
+    }
 }
