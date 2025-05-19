1460d5a5e0519c0dbb9676a495e2131e92cfeb5a
benisamuel <benisamuel566@gmail.com>
2025-05-19 13:08:52 +0200

    added playlistdto

--- src/main/java/com/b4music/b4core/dto/PlaylistDto.java
+++ src/main/java/com/b4music/b4core/dto/PlaylistDto.java
@@ -1,4 +1,11 @@
 package com.b4music.b4core.dto;
 
 public class PlaylistDto {
+    private String title;
+    private Long likes;
+    private Long disLikes;
+
+    public String getTitle () { return this.title; }
+    public Long getLikes () { return this.likes; }
+    public Long getDisLikes () { return this.disLikes; }
 }
