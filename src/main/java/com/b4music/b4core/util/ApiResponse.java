0cebbd3b603304b12137cc2d80621c45d3e3bc38
benisamuel <benisamuel566@gmail.com>
2025-08-29 18:41:21 +0200

    Added methods for proper api message response

--- src/main/java/com/b4music/b4core/util/ApiResponse.java
+++ src/main/java/com/b4music/b4core/util/ApiResponse.java
@@ -1,5 +1,8 @@
 package com.b4music.b4core.util;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class ApiResponse <T> {
     public boolean success;
     public String message;
@@ -9,5 +12,17 @@ public class ApiResponse <T> {
         this.success = success;
         this.message = message;
         this.data = data;
+    }
+
+    static <T> ResponseEntity<ApiResponse<T>> ok (String message, T data) {
+        return ResponseEntity.ok(new ApiResponse<T>(true, message, data));
+    }
+
+    static <T> ResponseEntity<ApiResponse<T>> notFound (String message, T data) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, message, data));
+    }
+
+    static <T> ResponseEntity<ApiResponse<T>> badRequest (String message, T data) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(false, message, data));
     }
 }
