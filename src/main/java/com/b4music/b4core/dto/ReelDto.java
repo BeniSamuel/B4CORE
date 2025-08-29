f2d711e12599c83b4ee162d82b8a060f26381f6b
benisamuel <benisamuel566@gmail.com>
2025-08-29 19:03:17 +0200

    used lombok in reels dto

--- src/main/java/com/b4music/b4core/dto/ReelDto.java
+++ src/main/java/com/b4music/b4core/dto/ReelDto.java
@@ -1,15 +1,10 @@
 package com.b4music.b4core.dto;
 
+import lombok.Getter;
+
+@Getter
 public class ReelDto {
     private String name;
     private String description;
-    private Long likes;
-    private Long dislikes;
     private Long user_id;
-
-    public String getName () { return this.name; }
-    public String getDescription () { return this.description; }
-    public Long getLikes () { return this.likes; }
-    public Long getDislikes () { return this.dislikes; }
-    public Long getUser_id () { return this.user_id; }
 }
