bd32aafe1bef7f0d662928dcf88f5dd20499280d
benisamuel <benisamuel566@gmail.com>
2025-08-29 18:43:47 +0200

    used lombok for injecting userservice and authenticationmanager

--- src/main/java/com/b4music/b4core/service/AuthService.java
+++ src/main/java/com/b4music/b4core/service/AuthService.java
@@ -4,6 +4,8 @@ import com.b4music.b4core.dto.UserLoginDto;
 import com.b4music.b4core.dto.UserRegisterDto;
 import com.b4music.b4core.model.User;
 import com.b4music.b4core.util.ApiResponse;
+import com.b4music.b4core.util.JwtUtil;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,14 +14,11 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
-
-    public AuthService (UserService userService, AuthenticationManager authenticationManager) {
-        this.userService = userService;
-        this.authenticationManager = authenticationManager;
-    }
+    private final JwtUtil jwtUtil;
 
     public User registerUser (UserRegisterDto userRegisterDto) {
         return this.userService.createUser(userRegisterDto);
@@ -30,6 +29,9 @@ public class AuthService {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(userLoginDto.getEmail(), userLoginDto.getPassword())
             );
+
+            User user = this.userService.getUserByEmail(userLoginDto.getEmail());
+            return ResponseEntity.ok(new ApiResponse<>(true, "Successfully logged in user!!!", jwtUtil.generateToken(user.getEmail())));
         } catch (AuthenticationException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(false, "Bad credentials!!!", "Bad credentials"));
         }
