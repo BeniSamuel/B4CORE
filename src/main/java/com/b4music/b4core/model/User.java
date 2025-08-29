bc1d1a35420fccab15729a24e73ac3b4bf963a3a
benisamuel <benisamuel566@gmail.com>
2025-08-29 19:00:51 +0200

    used lombok in user model

--- src/main/java/com/b4music/b4core/model/User.java
+++ src/main/java/com/b4music/b4core/model/User.java
@@ -2,25 +2,34 @@ package com.b4music.b4core.model;
 
 import com.b4music.b4core.enums.Role;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     @Column(unique = true)
     private String email;
+
     private String password;
+
+    @Column(unique = true)
     private int phone;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    public User () {}
     public User (String name, String email, String password, Role role) {
         this.name = name;
         this.email = email;
@@ -28,29 +37,4 @@ public class User {
         this.role = role;
     }
 
-    public String getName () { return this.name; }
-    public String getEmail () { return this.email; }
-    public String getPassword () { return this.password; }
-    public Role getRole () { return this.role; }
-    public int getPhone () { return this.phone; }
-
-    // Setters
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public void setPhone (int phone) {
-        this.phone = phone;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
