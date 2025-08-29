142b56604609673e3295ceda509a90f5105dc19d
benisamuel <benisamuel566@gmail.com>
2025-08-29 18:45:17 +0200

    used lombok to simplify model layout

--- src/main/java/com/b4music/b4core/model/Comment.java
+++ src/main/java/com/b4music/b4core/model/Comment.java
@@ -1,20 +1,24 @@
 package com.b4music.b4core.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Comment {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String message;
-    private Long likes;
-    private Long dislikes;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
-    private User user;
+    private User author;
 
     @ManyToOne
     @JoinColumn(name = "music_id")
@@ -28,95 +32,19 @@ public class Comment {
     @JoinColumn(name = "reels_id")
     private Reels reels;
 
-    @ManyToOne
-    @JoinColumn(name = "library_id")
-    private Library library;
-
-    public Comment () {}
-    public Comment (String message, Long likes, Long dislikes, Reels reels) {
+    public Comment (String message, Reels reels, User author) {
         this.message = message;
-        this.likes = likes;
-        this.dislikes = dislikes;
         this.reels = reels;
+        this.author = author;
     }
-    public Comment (String message, Long likes, long dislikes, Music music) {
+    public Comment (String message, Music music, User author) {
         this.message = message;
-        this.likes = likes;
-        this.dislikes = dislikes;
         this.music = music;
+        this.author = author;
     }
-    public Comment (String message, Long likes, Long dislikes, Playlists playlists) {
+    public Comment (String message, Playlists playlists, User author) {
         this.message = message;
-        this.likes = likes;
-        this.dislikes = dislikes;
         this.playlists = playlists;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public Long getLikes() {
-        return likes;
-    }
-
-    public Long getDislikes() {
-        return dislikes;
-    }
-
-    public String getMessage () {
-        return message;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Library getLibrary() {
-        return library;
-    }
-
-    public Music getMusic() {
-        return music;
-    }
-
-    public Playlists getPlaylists() {
-        return playlists;
-    }
-
-    public Reels getReels() {
-        return reels;
-    }
-
-    public void setUser (User user) {
-        this.user = user;
-    }
-
-    public void setMessage (String message) {
-        this.message = message;
-    }
-
-    public void setLikes (Long likes) {
-        this.likes = likes;
-    }
-
-    public void setDislikes (Long dislikes) {
-        this.dislikes = dislikes;
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void setReels(Reels reels) {
-        this.reels = reels;
-    }
-
-    public void setPlaylists(Playlists playlists) {
-        this.playlists = playlists;
-    }
-
-    public void setLibrary(Library library) {
-        this.library = library;
+        this.author = author;
     }
 }
