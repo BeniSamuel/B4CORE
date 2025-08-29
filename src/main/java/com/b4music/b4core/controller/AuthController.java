904986f9b459839039d0c8c95284b31d8025a018
benisamuel <benisamuel566@gmail.com>
2025-08-29 18:43:04 +0200

    used lombok for injecting authservice

--- src/main/java/com/b4music/b4core/controller/AuthController.java
+++ src/main/java/com/b4music/b4core/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.b4music.b4core.dto.UserRegisterDto;
 import com.b4music.b4core.model.User;
 import com.b4music.b4core.service.AuthService;
 import com.b4music.b4core.util.ApiResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/b4core/v1/auth")
+@AllArgsConstructor
 public class AuthController {
     private final AuthService authService;
-
-    public AuthController (AuthService authService) {
-        this.authService = authService;
-    }
 
     @PostMapping("/register-user")
     public ResponseEntity<ApiResponse<User>> registerUser (@RequestBody UserRegisterDto userRegisterDto) {
