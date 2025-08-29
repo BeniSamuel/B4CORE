0ebfb8602d00af4dc63cc303f63814df0500a9f8
benisamuel <benisamuel566@gmail.com>
2025-08-29 19:06:04 +0200

    added use of lombok on playlist dto

--- src/main/java/com/b4music/b4core/dto/PlaylistDto.java
+++ src/main/java/com/b4music/b4core/dto/PlaylistDto.java
@@ -1,11 +1,12 @@
 package com.b4music.b4core.dto;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class PlaylistDto {
     private String title;
-    private Long likes;
-    private Long disLikes;
-
-    public String getTitle () { return this.title; }
-    public Long getLikes () { return this.likes; }
-    public Long getDisLikes () { return this.disLikes; }
+    private Long user_id;
+    private final LocalDateTime created_at = LocalDateTime.now();
 }
