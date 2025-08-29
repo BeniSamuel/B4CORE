51e01f575d771684eefe31029af5cd7b1596d75a
benisamuel <benisamuel566@gmail.com>
2025-08-29 19:11:18 +0200

    used lombok to simplify dependency inejction

--- src/main/java/com/b4music/b4core/security/CustomUserDetailsService.java
+++ src/main/java/com/b4music/b4core/security/CustomUserDetailsService.java
@@ -3,18 +3,16 @@ package com.b4music.b4core.security;
 import com.b4music.b4core.exception.NotFoundException;
 import com.b4music.b4core.model.User;
 import com.b4music.b4core.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserService userService;
-
-    public CustomUserDetailsService (UserService userService) {
-        this.userService = userService;
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
