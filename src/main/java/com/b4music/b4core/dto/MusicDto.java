64770116b6359348864f0f25c2b4640f319b6b7b
benisamuel <benisamuel566@gmail.com>
2025-08-29 19:07:54 +0200

    Used lombok on music dto

--- src/main/java/com/b4music/b4core/dto/MusicDto.java
+++ src/main/java/com/b4music/b4core/dto/MusicDto.java
@@ -1,6 +1,10 @@
 package com.b4music.b4core.dto;
 
+import java.time.LocalDateTime;
+
 public class MusicDto {
     private String name;
-
+    private String description;
+    private Long user_id;
+    private final LocalDateTime added_at = LocalDateTime.now();
 }
