02015eb9d3b1bb4acb547e15abf7a1b49f1a389f
benisamuel <benisamuel566@gmail.com>
2025-08-29 19:02:57 +0200

    used lombok in reels model

--- src/main/java/com/b4music/b4core/model/Reels.java
+++ src/main/java/com/b4music/b4core/model/Reels.java
@@ -1,61 +1,33 @@
 package com.b4music.b4core.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@Setter
+@Getter
 public class Reels {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String description;
 
-    private Long likes;
-    private Long dislikes;
+    private String path;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
-    private User user;
+    private User author;
 
-    public Reels () {}
-    public Reels (String name, String description, Long likes, Long dislikes, User user) {
+
+    public Reels (String name, String description, User author) {
         this.name = name;
         this.description = description;
-        this.likes = likes;
-        this.dislikes = dislikes;
-        this.user = user;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public Long getDislikes() {
-        return dislikes;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public Long getLikes() {
-        return likes;
-    }
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setLikes(Long likes) {
-        this.likes = likes;
-    }
-    public void setDislikes (Long dislikes) {
-        this.dislikes = dislikes;
+        this.author = author;
     }
 }
